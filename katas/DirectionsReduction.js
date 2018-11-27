e6b08998843fe0b8711c6db030657846adb05ecd
Nick Bansal <Bansal321@nicks-imac.home>
2018-11-27 08:04:06 +0000

    Completed Directions Reduction

--- katas/DirectionsReduction.js
+++ katas/DirectionsReduction.js
@@ -1,14 +1,17 @@
-const DirectionsReduction = (array) => {
-    let x = 0, y = 0
-    const lookup = {'NORTH': 1, 'SOUTH': -1, 'WEST': 1, 'EAST': -1}
-    const total = array.reduce((acc, value) => {
-        (value === 'WEST' || value === 'EAST') ? y += lookup[value] : x += lookup[value]
-        if (y !== 0 || x !== 0) acc.push(value) 
-        else acc.pop()
-        return acc
-    }, [])
-    if (x === 0 && y === 0) return []
-    else return total
-}   
+const DirectionsReduction = (arr) => {
+    let i = 0;    
+    while (i < arr.length) {
+      if ((arr[i] === "NORTH" && arr[i+1] === "SOUTH") ||
+      (arr[i] === "SOUTH" && arr[i+1] === "NORTH") ||
+      (arr[i] === "EAST" && arr[i+1] === "WEST") ||
+      (arr[i] === "WEST" && arr[i+1] === "EAST")) {
+          arr.splice(i, 2);
+          i--;
+      } else {
+          i++;
+      }
+    }
+    return arr;
+  } 
 
 module.exports = DirectionsReduction
