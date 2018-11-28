a06fb6855ffc52472af16b875879c144fd1b4d6a
Nick Bansal <Bansal321@nicks-imac.home>
2018-11-28 11:24:29 +0000

    Cleared up

--- katas/CalculatingwithFunctions.js
+++ katas/CalculatingwithFunctions.js
@@ -1,41 +1,19 @@
-const expr = (number, operation) => {
-    return (!operation) ? number : operation(number)
-}
+const expr  = (number, operation) => (!operation) ? number : operation(number)
 
-const zero = (operand) => {
-    return expr(0, operand)
-}
-const one = operand => {
-    return expr(1, operand)
-}
-const two = (operand) => {
-    return expr(2, operand)
-}
-const three = (operand) => {
-    return expr(3, operand)
-}
-const four = (operand) => {
-    return expr(4, operand)
-}
-const five = (operand) => {
-    return expr(5, operand)
-}
-const six = (operand) => {
-    return expr(6, operand)
-} 
-const seven = (operand) => {
-    return expr(7, operand)
-} 
-const eight = (operand) => {
-    return expr(8, operand)
-} 
-const nine = (operand) => {
-    return expr(9, operand)
-} 
+const zero  = operand => expr(0, operand)
+const one   = operand => expr(1, operand)
+const two   = operand => expr(2, operand)
+const three = operand => expr(3, operand)
+const four  = operand => expr(4, operand)
+const five  = operand => expr(5, operand)
+const six   = operand => expr(6, operand)
+const seven = operand => expr(7, operand)
+const eight = operand => expr(8, operand)
+const nine  = operand => expr(9, operand)
 
-const plus = a => b => b + a
-const minus = a => b => b - a;
-const times = a => b => b * a;
+const plus      = a => b => b + a
+const minus     = a => b => b - a;
+const times     = a => b => b * a;
 const dividedBy = a => b => Math.floor(b / a)
 
 
