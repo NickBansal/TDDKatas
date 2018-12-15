f6b1dc72b37aa1b68f3f9eebfe9f81a20de418f7
Nick Bansal <https://github.com/NickBansal>
2018-12-15 14:21:05 +0000

    All tests passed

--- katas/ConsecutiveStrings.js
+++ katas/ConsecutiveStrings.js
@@ -1,8 +1,23 @@
 const longestConsec = (array, k) => {
     const n = array.length
     if (n === 0 || n < k) return ""
+    return array.reduce((acc, value, index) => {
+        if (index === 0) {
+            for (let i=0; i<k; i++) {
+                acc.push(array[index + i])
+            }
+        }
+        if (index > 0 && index <= array.length - k) {
+            let checker = []
+            for (let i=0; i<k; i++) {
+                checker.push(array[index + i])
+            }
+            if (checker.join('').length > acc.join('').length) {
+                acc = checker
+            }
+        }
+        return acc
+    }, []).join('')
 }
-
-// n being the length of the string array, if n = 0 or k > n or k <= 0 return "".
 
 module.exports = longestConsec
--- spec/ConsecutiveStrings.spec.js
+++ spec/ConsecutiveStrings.spec.js
@@ -8,4 +8,13 @@ describe.only('Consecutive Strings', () => {
     it('If the length af the given array is less then k return empty string', () => {
         expect(longestConsec(['a', 'b'], 3)).to.equal("")
     })
+    it('If second argument is <= 0 return empty string', () => {
+        expect(longestConsec([], 0)).to.equal("")
+    })
+    it('Given an array of strings, return the longest consecutive strings', () => {
+        expect(longestConsec(['a', 'bc', 'def'], 2)).to.equal("bcdef")
+    })
+    it('Given an array of strings, return the longest consecutive strings', () => {
+        expect(longestConsec(['abc', 'd', 'efgh', 'ij', 'klmno'], 3)).to.equal('efghijklmno')
+    })
 })
