552e075d94338f50fa8f469402bf0fcf492a7bdc
Nick Bansal <https://github.com/NickBansal>
2018-12-20 15:34:42 +0000

    Doors added tested

--- katas/BuildaCar.js
+++ katas/BuildaCar.js
@@ -1,26 +1,39 @@
 class Vehicle {
-    constructor(length) {
-        let layer3
-        const layer1 = ` ${'-'.repeat(length - 2)}\n`
-        if (length > 11) {
-            let i = 11
-            let backWheel = ''
-            let frontWheel = ''
-            let x = 9
-            let front = true
-            while (i <= length) {
-                front ? backWheel += 'o-' : frontWheel += '-o'
-                layer3 = `-o-${backWheel}${'-'.repeat(length - x)}${frontWheel}-o-'`
+    constructor(length, door) {
+        let layer3, 
+        layer2, 
+        layer1 = ` ${'-'.repeat(length - 2)}\n`
+        
+        if (door > 0) {
+            let j = 1, i = 5, backDoor = '', frontDoor = '', front = true
+            while(j <= door) {
+                front ? frontDoor += '[]' : backDoor += '[]'
+                layer2 = `|${backDoor}${' '.repeat(length - i)}${frontDoor}\\`
+                j++
                 i+=2
-                x+=2
                 front = !front
+            }
+        }
+
+        if (length > 11) {
+            let i = 12,
+            backWheel = '', frontWheel = '',
+            back = true
+            while (i <= length) {
+                back ? backWheel += 'o-' : frontWheel += '-o'
+                layer3 = `-o-${backWheel}${'-'.repeat(length - (i-3))}${frontWheel}-o-'`
+                i+=2
+                back = !back
             }
         } else {
             layer3 = `-o-${'-'.repeat(length - 7)}-o-'`
         }
         
         this.body = {
-            component: { layer1 }
+            component: { 
+                layer1, 
+                layer2 
+            }
         }
         this.chassis = {
             component: { layer3 }
@@ -28,10 +41,10 @@ class Vehicle {
     }
 }
 
-const car = (length) => {
+const car = (length, door) => {
     let Car
     if (length < 7) throw new RangeError('length must be larger then 7');
-    else Car = new Vehicle(length)
+    else Car = new Vehicle(length, door)
     return Car
 }
 
--- spec/BuildaCar.spec.js
+++ spec/BuildaCar.spec.js
@@ -11,16 +11,19 @@ describe.only('Build a Car', () => {
         expect(car(11).body.component.layer1).to.equal(' ---------\n')
     })
     it('if length < 7 it throws an error', () => {
-        expect(() => car(0)).to.throw()
-        expect(() => car(2)).to.throw(RangeError, 'length must be larger then 7')
+        expect(() => car(0, 1)).to.throw()
+        expect(() => car(2, 1)).to.throw(RangeError, 'length must be larger then 7')
     })
-    it.only('Returns the correct amount of axels and wheels to the bottom layer', () => {
-        expect(car(7).chassis.component.layer3).to.equal("-o--o-'")
-        expect(car(11).chassis.component.layer3).to.equal("-o------o-'")
-        expect(car(12).chassis.component.layer3).to.equal("-o-o-----o-'")
-        expect(car(13).chassis.component.layer3).to.equal("-o-o----o-o-'")
+    it('Returns the correct amount of axels and wheels to the bottom layer', () => {
+        expect(car(7, 0).chassis.component.layer3).to.equal("-o--o-'")
+        expect(car(11, 0).chassis.component.layer3).to.equal("-o------o-'")
+        expect(car(12, 0).chassis.component.layer3).to.equal("-o-o-----o-'")
+        expect(car(13).chassis.component.layer3).to.equal("-o-o------o-'")
         expect(car(16).chassis.component.layer3).to.equal("-o-o-o-----o-o-'")
+        expect(car(17).chassis.component.layer3).to.equal("-o-o-o------o-o-'")
+    })
+    it.only('Returns the correct amount of doors and spaces', () => {
+        expect(car(7, 1).body.component.layer2).to.equal("|  []\\")
+        expect(car(7, 2).body.component.layer2).to.equal("|[][]\\")
     })
 })
-
-// new Error('Divide by zero is not permitted.')
