33651f62c1ecad894caa65bdf212da4537015ff4
Nick Bansal <https://github.com/NickBansal>
2018-12-20 14:52:14 +0000

    layer 3 tested

--- katas/BuildaCar.js
+++ katas/BuildaCar.js
@@ -1,20 +1,38 @@
 class Vehicle {
     constructor(length) {
-        const layer1 = ` ${'-'.repeat(length-1)}`
+        let layer3
+        const layer1 = ` ${'-'.repeat(length - 2)}\n`
+        if (length > 11) {
+            let i = 11
+            let backWheel = ''
+            let frontWheel = ''
+            let x = 9
+            let front = true
+            while (i <= length) {
+                front ? backWheel += 'o-' : frontWheel += '-o'
+                layer3 = `-o-${backWheel}${'-'.repeat(length - x)}${frontWheel}-o-'`
+                i+=2
+                x+=2
+                front = !front
+            }
+        } else {
+            layer3 = `-o-${'-'.repeat(length - 7)}-o-'`
+        }
+        
         this.body = {
             component: { layer1 }
         }
-        // this.chassis = {
-        //     component: { layer3 }
-        // }
+        this.chassis = {
+            component: { layer3 }
+        }
     }
 }
 
 const car = (length) => {
     let Car
-    if (length < 7) throw new Error('length must be larger then 7');
+    if (length < 7) throw new RangeError('length must be larger then 7');
     else Car = new Vehicle(length)
     return Car
 }
 
-module.exports = car
+module.exports = { car, Vehicle }
--- spec/BuildaCar.spec.js
+++ spec/BuildaCar.spec.js
@@ -1,14 +1,25 @@
-const car = require('../katas/BuildaCar')
+const { car, Vehicle } = require('../katas/BuildaCar')
 const { expect } = require('chai')
 
 describe.only('Build a Car', () => {
+    it('Creates a new instance of the class', () => {
+        expect(new Vehicle()).to.be.an.instanceof(Vehicle);
+    })
     it('Returns the correct amount of top layer', () => {
-        expect(car(7).body.component.layer1).to.equal(' ------')
-        expect(car(9).body.component.layer1).to.equal(' --------')
-        expect(car(11).body.component.layer1).to.equal(' ----------')
+        expect(car(7).body.component.layer1).to.equal(' -----\n')
+        expect(car(9).body.component.layer1).to.equal(' -------\n')
+        expect(car(11).body.component.layer1).to.equal(' ---------\n')
     })
     it('if length < 7 it throws an error', () => {
-        expect(car(0)).to.throw(Error)
+        expect(() => car(0)).to.throw()
+        expect(() => car(2)).to.throw(RangeError, 'length must be larger then 7')
+    })
+    it.only('Returns the correct amount of axels and wheels to the bottom layer', () => {
+        expect(car(7).chassis.component.layer3).to.equal("-o--o-'")
+        expect(car(11).chassis.component.layer3).to.equal("-o------o-'")
+        expect(car(12).chassis.component.layer3).to.equal("-o-o-----o-'")
+        expect(car(13).chassis.component.layer3).to.equal("-o-o----o-o-'")
+        expect(car(16).chassis.component.layer3).to.equal("-o-o-o-----o-o-'")
     })
 })
 
