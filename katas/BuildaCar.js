99d2868e3a3d7ef41f277a9273f786efd39b65d9
Nick Bansal <https://github.com/NickBansal>
2018-12-20 12:30:58 +0000

    Build a car testing errors

--- katas/BuildaCar.js
+++ katas/BuildaCar.js
@@ -1,5 +1,20 @@
-const car = (length, doors) => {
-    // engineer your cars
+class Vehicle {
+    constructor(length) {
+        const layer1 = ` ${'-'.repeat(length-1)}`
+        this.body = {
+            component: { layer1 }
+        }
+        // this.chassis = {
+        //     component: { layer3 }
+        // }
+    }
+}
+
+const car = (length) => {
+    let Car
+    if (length < 7) throw new Error('length must be larger then 7');
+    else Car = new Vehicle(length)
+    return Car
 }
 
 module.exports = car
--- spec/BuildaCar.spec.js
+++ spec/BuildaCar.spec.js
@@ -2,7 +2,14 @@ const car = require('../katas/BuildaCar')
 const { expect } = require('chai')
 
 describe.only('Build a Car', () => {
-    it('return 0', () => {
-        expect(car(0)).to.equal(0)
+    it('Returns the correct amount of top layer', () => {
+        expect(car(7).body.component.layer1).to.equal(' ------')
+        expect(car(9).body.component.layer1).to.equal(' --------')
+        expect(car(11).body.component.layer1).to.equal(' ----------')
+    })
+    it('if length < 7 it throws an error', () => {
+        expect(car(0)).to.throw(Error)
     })
 })
+
+// new Error('Divide by zero is not permitted.')
