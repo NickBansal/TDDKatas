ea485ca4aab2e7125313a671923fa86c8acd6d0a
Nick Bansal <https://github.com/NickBansal>
2018-12-20 11:31:17 +0000

    Rot 13 complete

--- katas/MaxSubArraySum.js
+++ katas/MaxSubArraySum.js
@@ -1,5 +1,12 @@
-const maxSequence = arr => {
-
+const maxSequence = array => {
+    let currentScore = 0;
+    let maxScore = 0;
+    array.forEach(element => {
+        maxScore += element
+        if (maxScore < 0) maxScore = 0;
+        if (currentScore < maxScore) currentScore = maxScore
+    })
+    return currentScore;
 }
 
 module.exports = maxSequence
---
+++ katas/Rot13.js
@@ -0,0 +1,15 @@
+const rot13 = (message) => {  
+    return message.split('').reduce((acc, value) => {
+        let characterCode = value.charCodeAt(0)
+        
+        let newValue = /[a-z]/.test(value) && characterCode + 13 > 122
+        || /[A-Z]/.test(value) && characterCode + 13 > 90 ? (characterCode + 13) - 26 :
+        !/[a-z]/i.test(value) ? characterCode : characterCode + 13
+
+        acc += String.fromCharCode(newValue)
+        return acc
+    }, '')
+}
+
+module.exports = rot13
+
